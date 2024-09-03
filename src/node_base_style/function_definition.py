b733ecf735636162f2c7c6d94dd4124a16bfb306
Tairan <1739506560@qq.com>
2024-09-03 12:25:39 +0800

    slightly change prompt

--- src/node_base_style/function_definition.py
+++ src/node_base_style/function_definition.py
@@ -4,7 +4,8 @@ from node_base_style.hoare_triple import FuncTriple
 from node_base_style.helper import extract_result
 
 PROMPT = """
-You have been assigned the task of a program verifier, responsible for describing the functionality of a Python function. You will be provided with the constraints and relationships between the input parameters, as well as the resulting output of the function based on these inputs. Your task is to organize this information and describe the parameters the function accepts, its return value, and any side effects. Please avoid describing how the function operates or details such as how local variables are initialized. You must adhere to the text format: Functionality: **functionality** and Side Effects: **side effects**.
+You have been assigned the task of a program verifier, responsible for describing the functionality of a Python function. You will be provided with the constraints and relationships between the input parameters, as well as the resulting output of the function based on these inputs. Your task is to organize this information and describe the functionality of the function. Please avoid describing how the function operates or details such as how local variables are initialized—only describe what parameters the function accepts and what it returns. You must adhere to the text format: Functionality: **functionality**.
+
 
 Example:
 Parameter constraints and relationships: `number` is an integer.
@@ -12,13 +13,12 @@ Parameter constraints and relationships: `number` is an integer.
 def func(number):
 ```
 Output: `number` is an integer. If `number` is even, the function returns True; otherwise, it returns False.
-Now, please think step by step: What are the parameters the function accepts, the return value, and the side effects?
+Now, please think step by step: What are the parameters the function accepts, and the return value?
 
 
 Example Answer:
 The function `func` accepts a parameter `number`. `number` is an integer. After executing the function body, if number is even, the function returns `True`; otherwise, it returns `False`. Therefore, the functionality of the function func is to accept an integer `number`, and if `number` is even, it returns True. If `number` is not even, it returns False.
 Functionality: **The function accepts a parameter number, returns `True` if `number` is even. If `number` is not even, it returns `False`.**
-Side effect: **No side effect**
 
 
 Your Task:
@@ -27,7 +27,7 @@ Parameter constraints: {pre}
 {head}
 ```
 Output: {body_post}
-Now, please think step by step: What are the parameters the function accepts, the return value, and the side effects?
+Now, please think step by step: What are the parameters the function accepts, and the return value?
 """
 
 
