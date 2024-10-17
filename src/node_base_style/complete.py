96c9110b5070b1a909cc3d8c70670d8b0a64e08c
jimbou <jimbou1999@gmail.com>
2024-10-17 14:25:59 +0800

    added sense of depth of recursion (for future state merging) and improved prompts

--- src/node_base_style/complete.py
+++ src/node_base_style/complete.py
@@ -14,9 +14,12 @@ class PostconditionAnalyzer:
     def __init__(self, model, config):
         self.model = model
         self.config = config
-        self.collected_returns = []  # Collects postconditions from return statements as strings
+        # Collects postconditions from return statements as strings. but i also want it to collect the depth for each postcondition so a list of tuples
+        self.collected_returns = []
+        self.collected=[]
         self.got_return = False # Flag to check if a return statement was encountered in the same level of recursion
         self.last_return =""
+        self.last_reurn_depth=0
     # Core recursive method to compute the postcondition of a Triple .
     def complete_triple_cot(self, triple: Triple, depth=0) -> str:
         assert triple.postcondition == State.UNKNOWN
@@ -25,15 +28,18 @@ class PostconditionAnalyzer:
         if isinstance(triple.command,
                       (ast.Assign, ast.AugAssign, ast.Expr, ast.Raise, ast.Pass, ast.Break, ast.Continue)):
             post = complete_triple(triple, self.model)
+            self.collected.append((str(post), depth))
             return post
 
         # Handle return statements
         if isinstance(triple.command, ast.Return):
             post = complete_triple(triple, self.model)
             # If we're at the first level of depth (inside a function), collect the return postcondition and we are done for this recursion
-            if depth == 1 :
+            if depth >= 1 :
                 self.got_return = True
                 self.last_return = str(post)
+                self.last_reurn_depth= depth
+                self.collected.append((str(post), depth))
                 #self.collected_returns.append(str(post))
             return post
 
@@ -44,6 +50,7 @@ class PostconditionAnalyzer:
             for subcmd in triple.command:
                 completion = self.complete_triple_cot(Triple(pre, subcmd, State.UNKNOWN), depth=depth)
                 pre = completion
+                #self.collected.append((str(completion), depth))
                 if self.got_return:
                     break
             return pre
@@ -52,24 +59,26 @@ class PostconditionAnalyzer:
         if isinstance(triple.command, ast.If):
             pre = triple.precondition
             # Find the postcondition for the if body
-            then_completion = self.complete_triple_cot(Triple(pre, triple.command.body, State.UNKNOWN), depth=depth)
+            then_completion = self.complete_triple_cot(Triple(pre, triple.command.body, State.UNKNOWN), depth=depth+1)
             if_post = then_completion
 
             else_post = None
             if triple.command.orelse:
                 # If there is an else part, find the postcondition for it
                 else_completion = self.complete_triple_cot(Triple(pre, triple.command.orelse, State.UNKNOWN),
-                                                           depth=depth)
+                                                           depth=depth+1)
                 else_post = else_completion
 
             # Create an IfTriple to represent the if statement with its branches and then compute the overall post condition
             if_triple = IfTriple(pre, triple.command, if_post, else_post, State.UNKNOWN)
             post = complete_if_triple(if_triple, self.model)
+            self.collected.append((str(post), depth))
             # If we are inside a function and there's a return statement, collect the postcondition and we are done for this recursion
-            if depth == 1 and any(isinstance(node, ast.Return) for node in ast.walk(triple.command)):
+            if depth >= 1 and any(isinstance(node, ast.Return) for node in ast.walk(triple.command)):
                 self.got_return = False
-                print("got return at if")
-                self.collected_returns.append(str(post))
+                self.collected_returns.append((str(post),self.last_reurn_depth))
+                self.last_reurn_depth=0
+
                 # since the post condition has been appended as a return element we dont deal with it any more
                 return pre
             return post
@@ -80,18 +89,20 @@ class PostconditionAnalyzer:
             try_command = triple.command.body # Commands inside the try block
             except_command = triple.command.handlers[0].body # Commands inside the first except block
             # First get the postcondition for the try block
-            try_completion = self.complete_triple_cot(Triple(pre, try_command, State.UNKNOWN), depth=depth)
+            try_completion = self.complete_triple_cot(Triple(pre, try_command, State.UNKNOWN), depth=depth+1)
             # Then get the postcondition for the except block
             except_completion = self.complete_triple_cot(Triple(State.UNKNOWN, except_command, State.UNKNOWN),
-                                                         depth=depth)
+                                                         depth=depth+1)
             # Create a TryTriple to represent the try-except block and then compute the overall postcondition
             try_triple = TryTriple(pre, triple.command, try_command, try_completion, except_command, except_completion,
                                    State.UNKNOWN)
 
             post = complete_try_triple(try_triple, self.model)
+            self.collected.append((str(post), depth))
             # If we are inside a function and there's a return statement, collect the postcondition and we are done for this recursion
-            if depth == 1 and any(isinstance(node, ast.Return) for node in ast.walk(triple.command)):
-                self.collected_returns.append(str(post))
+            if depth >= 1 and any(isinstance(node, ast.Return) for node in ast.walk(triple.command)):
+                self.collected_returns.append((str(post),self.last_reurn_depth))
+                self.last_reurn_depth=0
                 self.got_return = False
                  # since the post condition has been appended as a return element we dont deal with it any more
                 return pre
@@ -111,15 +122,20 @@ class PostconditionAnalyzer:
             examples = []
             pre = triple.precondition
             # Unroll the loop by simulating 'k' iterations
+            depth = depth + 1
             for _ in range(k):
                 post = self.complete_triple_cot(Triple(pre, body_command, State.UNKNOWN), depth=depth)
+                self.collected.append((str(post), depth))
                 examples.append(Triple(pre, body_command, post))
                 pre = get_precondition(self.model, post, while_head)
+            depth = depth -1
             # Create a Triple for the entire 'while' loop
             triple = Triple(triple.precondition, triple.command, State.UNKNOWN)
             post = complete_loop_triple(triple, self.model, examples)
-            if depth == 1 and any(isinstance(node, ast.Return) for node in ast.walk(triple.command)):
-                self.collected_returns.append(post)
+            self.collected.append((str(post), depth))
+            if depth >= 1 and any(isinstance(node, ast.Return) for node in ast.walk(triple.command)):
+                self.collected_returns.append((str(post),self.last_reurn_depth))
+                self.last_reurn_depth=0
                 self.got_return = False
                  # since the post condition has been appended as a return element we dont deal with it any more
                 return pre
@@ -136,21 +152,32 @@ class PostconditionAnalyzer:
 
             #if the got_return flag is True then the last_return ahs not been appended to the collected_returns
             if self.got_return:
-                print("got return")
-                self.collected_returns.append(self.last_return)
+                self.collected_returns.append((self.last_return,self.last_reurn_depth))
                 self.got_return = False
             if len(self.collected_returns) > 1:
-                # add Case_{counter} to ecah return postcondition and new line at the end of it
-                self.collected_returns= [f"Case_{i+1}: {ret}" for i, ret in enumerate(self.collected_returns)]
+                # add Case_{counter} to ecah return postcondition and new line at the end of it. but remember that the collected_returns is a list of tuples
+                # also i want as many * as the depth of the return
+                # self.collected_returns = [f"{'    ' * int(ret[1])} Case_{i+1}: {ret[0]}\n" for i, ret in enumerate(self.collected_returns)]
+                self.collected_returns = [f"Case_{i+1}: {ret[0]}\n" for i, ret in enumerate(self.collected_returns)]
+                # self.collected_returns= [f"Case_{i+1}: {ret}" for i, ret in enumerate(self.collected_returns)]
             return_conditions_str = "\n".join(self.collected_returns)
-            print("return_conditions_str", return_conditions_str)
-            #print the self.collected_returns with /n as a separator
-            #print("@@@@@@@@@@\n".join(self.collected_returns))
+            print("return_conditions_str\n", return_conditions_str)
+           
 
             func_triple = FuncTriple(triple.precondition, triple.command, def_str, triple.command.body,
                                     return_conditions_str, State.UNKNOWN)
             #this gives us the functionality of the function
-            return complete_func_triple(func_triple, self.model)
+            final= complete_func_triple(func_triple, self.model)
+            self.collected.append((str(final), depth))
+            # in the self.collected list give to its item a name. The name must be their depth and then a . aand then the number of which eleemnth with depth = thir depth exist in this list
+
+            # self.collected = [f"{'    ' * int(ret[1])} Case_{i+1}: {ret[0]}\n" for i, ret in enumerate(self.collected)]
+            # #print the self.collected items one by one
+            # print("self.collected\n:")
+            # for i in self.collected:
+            #     print(i)
+            
+            return final
          
         # Handle import statements and assertions as they dont change the state
         if isinstance(triple.command, (ast.Import, ast.ImportFrom, ast.Assert)):
--- src/node_base_style/general.py
+++ src/node_base_style/general.py
@@ -6,7 +6,7 @@ from node_base_style.helper import extract_result
 
 # This script's responsible for executing small code snippets and determining the resulting program state based on the provided initial state and program code. It is the general script for a simple program statement (not loops or ifs, try etc)
 PROMPT = """
-You have been assigned the role of a program executor, responsible for simulating the execution of Python code. You will be provided with an initial state and a Python code snippet. You need to provide the output state after running the Python code based on the initial state. Please avoid describing how the program runs. When a variable has a specific value, use that specific value directly for calculations. If a return takes place makes sure to always include the return value or variable at the output state. You must adhere to the text format: Output State: **output state**.
+You have been assigned the role of a program executor, responsible for simulating the execution of Python code. You will be provided with an initial state and a Python code snippet. You need to provide the output state after running the Python code based on the initial state. Please avoid describing how the program runs. When a variable has a specific value, use that specific value directly for calculations. If a return takes place makes sure to always mention that a value or variable has been returned at the output state. You must adhere to the text format: Output State: **output state**.
 
 
 Example1:
@@ -63,6 +63,15 @@ Example Answer:
 The function returns `n`, and `n` has a value of 0, so the function returns 0. The return statement does not change the state, so the output state is `n` is 0, `m` is 2, and the function returns 0.
 Output State: **`n` is 0, `m` is 2, and the function returns 0**
 
+Example6:
+Initial State: `x` is 0, `y` is 1
+```
+return 1
+```
+Now, please think step by step: List the impact of the code on the program, check the previous values of the affected variables, and then calculate the result.
+Example Answer: 
+The function returns `1`, and `x` has a value of 0, and y has a value of 1. The return statement does not change the state, so the output state is `x` is 0, `y` is 2, and the function returns 1.
+Output State: **`x` is 0, `y` is 1, and the function returns 1**
 
 Your Task:
 Initial State: {pre}
