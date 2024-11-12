76830a12ee153483ba2c38a0e64c23b34fa9dccb
jimbou <jimbou1999@gmail.com>
2024-11-12 11:55:35 +0800

    fixed the annotations for the return statements

--- diagonals/normal_20/program_cleaned.py
+++ diagonals/normal_20/program_cleaned.py
@@ -1,16 +1,7 @@
-def func_1(n: int) -> int:
-
-    def is_prime(num: int) -> bool:
-        if num < 2:
-            return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-    count = 0
-    for num in range(2, n):
-        if is_prime(num):
-            count += 1
-    return count
-print('SAA')
-print(func_1(10))
+def func_1(matrix):
+    n = len(matrix)
+    total = 0
+    for i in range(n):
+        total += matrix[i][i]
+        total += matrix[i][n - i - 1]
+    return total
