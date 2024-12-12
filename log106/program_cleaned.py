e9c24ffec4e8194b613ad6671baeb2b4e3dec8c1
jimbou <jimbou1999@gmail.com>
2024-12-12 13:31:12 +0800

    restructured and cleaned up repo

--- log106/program_cleaned.py
+++
@@ -1,8 +0,0 @@
-def func_1(matrix):
-    n = len(matrix)
-    total = 0
-    for i in range(n):
-        total += matrix[i][i]
-        total += matrix[i][n - i - 1]
-    return total
-print(func_1([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
